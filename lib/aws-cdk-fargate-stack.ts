43c152be2bb5522d6a726045351dfa825f842778
mostafa-bs23 <mostafa.mahmud@brainstation-23.com>
2023-01-16 15:44:04 +0600

    VPC creation

--- lib/aws-cdk-fargate-stack.ts
+++ lib/aws-cdk-fargate-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';x
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class AwsCdkFargateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +9,28 @@ export class AwsCdkFargateStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    const vpc = new ec2.Vpc(this, "FargateNodeJsVpc", {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "ingress",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "application",
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        },
+      ],
+    });
+
     // example resource
     // const queue = new sqs.Queue(this, 'AwsCdkFargateQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
   }
 }
+
+
