87682d24ec2e97ec33e47f2b7d16546ab1bf025f
mostafa-bs23 <mostafa.mahmud@brainstation-23.com>
2023-01-16 15:48:40 +0600

    PRIVATE_ISOLATED used instead

--- lib/aws-cdk-fargate-stack.ts
+++ lib/aws-cdk-fargate-stack.ts
@@ -2,12 +2,18 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';x
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export class AwsCdkFargateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsCdkFargateQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
 
     const vpc = new ec2.Vpc(this, "FargateNodeJsVpc", {
       maxAzs: 2,
@@ -26,10 +32,13 @@ export class AwsCdkFargateStack extends cdk.Stack {
       ],
     });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkFargateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const loadbalancer = new ApplicationLoadBalancer(this, "lb", {
+      vpc,
+      internetFacing: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PUBLIC,
+      }),
+    });
   }
 }
 
